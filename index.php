d481e578bbaa9cc2a3c36eadcba292312b7f3b4b
jerome <jeromeginouvez@gmail.com>
2022-05-02 15:44:14 +0200

    footer-fini

--- index.php
+++ index.php
@@ -6,25 +6,23 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+        
         <script src="bootstrap/js/bootstrap.min.js" async></script>
     </head>
     <body>
 
-        <main>
-            <section id="presentation">
-            </section>
+            <main>
+                <section id="presentation">
+                </section>
 
-            <section id="grille">
-                <div class="row">
-                    <!--La classe bg-info ajoute un fond bleu à l'élément-->
-                    <div class="col bg-info">1er élément</div>
-                    <!--La classe bg-warning ajoute un fond jaune à l'élément-->
-                    <div class="col bg-warning">2è élément</div>
-                    <!--La classe bg-success ajoute un fond vert à l'élément-->
-                    <div class="col bg-success">3è élément</div>
-                </div>
-            </section>
-        </main>
-        
+                <section id="grille">
+                    
+                </section>
+            </main>
+        <footer>
+         <?php
+         include 'PHP/footer.php'
+         ?>
+        </footer>
     </body>
 </html>
