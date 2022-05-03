18b6e9ee5b99f0e504bccc71dcf0356d44215c4a
jerome <jeromeginouvez@gmail.com>
2022-05-03 10:05:53 +0200

    ajout police

--- PHP/tableau-categorie.php
+++ PHP/tableau-categorie.php
@@ -1,27 +1,36 @@
 <link rel="stylesheet" href="style/tableau-categorie.css">
+<link rel="stylesheet" href="style\font.css">
 <div class="contener-categorie">
-    <div class="corde-frotté">
+    <div class="corde-frotté instru">
+    <p>Corde frottées</p>
     <img src="picture/instruments-cordes-frottees.jpg" alt="instruments-cordes-frottees">
     </div>
-    <div class="corde-pincé">
+    <div class="corde-pincé instru">
+    <p>Corde pincées</p>
     <img src="picture/instruments-cordes-pincees.jpg" alt="instruments-cordes-pincees">
     </div>
-    <div class="corde-frappé">
+    <div class="corde-frappé instru">
+    <p>Corde frappées</p>
     <img src="picture\instruments-cordes-frappées.jpg" alt="instruments-cordes-frappees">
     </div>
-    <div class="bois">
+    <div class="bois instru">
+    <p>Bois</p>
     <img src="picture\instruments-bois.jpg" alt="bois"> 
     </div>
-    <div class="cuivre">
+    <div class="cuivre instru">
+    <p>Cuivre</p>
     <img src="picture\instruments-cuivre.jpg" alt="cuivre">
     </div>
-    <div class="percussion-clavier">
+    <div class="percussion-clavier instru">
+    <p>Percussion clavier</p>
     <img src="picture\instruments-percussion-clavier.jpg" alt="percussion-clavier">
     </div>
-    <div class="percussion-peau">
+    <div class="percussion-peau instru">
+    <p>Percussion peau</p>
     <img src="picture\instruments-percussion-peau.jpg" alt="percussion-peau">
     </div>
-    <div class="percussion-accessoire">
+    <div class="percussion-accessoire instru">
+        <p>Percussion accessoire</p>
     <img src="picture\instruments-percussion-accessoire.jpg" alt="percussion-accessoire">
     </div>
 </div>
