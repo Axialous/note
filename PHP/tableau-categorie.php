0e6fc531729f615055ad2a064577efe19145c18d
jerome <jeromeginouvez@gmail.com>
2022-05-03 09:02:34 +0200

    02/05

---
+++ PHP/tableau-categorie.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" href="style/tableau-categorie.css">
+<div class="contener-categorie">
+    <div class="corde-frotté">
+    <img src="picture/instruments-cordes-frottees.jpg" alt="instruments-cordes-frottees">
+    </div>
+    <div class="corde-pincé">
+    <img src="picture/instruments-cordes-pincees.jpg" alt="instruments-cordes-pincees">
+    </div>
+    <div class="corde-frappé">
+    <img src="picture\instruments-cordes-frappées.jpg" alt="instruments-cordes-frappees">
+    </div>
+    <div class="bois">
+    <img src="picture\instruments-bois.jpg" alt="bois"> 
+    </div>
+    <div class="cuivre">
+    <img src="picture\instruments-cuivre.jpg" alt="cuivre">
+    </div>
+    <div class="percussion-clavier">
+    <img src="picture\instruments-percussion-clavier.jpg" alt="percussion-clavier">
+    </div>
+    <div class="percussion-peau">
+    <img src="picture\instruments-percussion-peau.jpg" alt="percussion-peau">
+    </div>
+    <div class="percussion-accessoire">
+    <img src="picture\instruments-percussion-accessoire.jpg" alt="percussion-accessoire">
+    </div>
+</div>
--- index.php
+++ index.php
@@ -5,9 +5,6 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-        
-        <script src="bootstrap/js/bootstrap.min.js" async></script>
     </head>
     <body>
 
@@ -16,7 +13,9 @@
                 </section>
 
                 <section id="grille">
-                    
+                <?php
+                include 'PHP/tableau-categorie.php'
+                ?>   
                 </section>
             </main>
         <footer>
