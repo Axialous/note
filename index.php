bd23b7a780a179874d66bb24b45747e72abc61ac
Alexis Bonnet <a.bonnet@codeur.online>
2022-05-03 14:20:54 +0200

    en-tête

--- index.php
+++ index.php
@@ -1,28 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
     <head>
         <title>Document</title>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-        <script src="bootstrap/js/bootstrap.min.js" async></script>
+        <link rel="stylesheet" href="style/header.css">
+        <script src="JS/JavaSript.js"></script>
     </head>
     <body>
+
+        <?php
+        include 'PHP/header.php';
+        ?>
 
         <main>
             <section id="presentation">
             </section>
 
             <section id="grille">
-                <div class="row">
-                    <!--La classe bg-info ajoute un fond bleu à l'élément-->
-                    <div class="col bg-info">1er élément</div>
-                    <!--La classe bg-warning ajoute un fond jaune à l'élément-->
-                    <div class="col bg-warning">2è élément</div>
-                    <!--La classe bg-success ajoute un fond vert à l'élément-->
-                    <div class="col bg-success">3è élément</div>
-                </div>
             </section>
         </main>
         
