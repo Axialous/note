f1405bbe5bf7a5bb4e1a6cda58daf80f3597e934
jerome <jeromeginouvez@gmail.com>
2022-05-03 14:20:23 +0200

    identification fini

--- PHP/identification.php
+++ PHP/identification.php
@@ -1,1 +1,19 @@
-bonjour
+<link rel="stylesheet" href="../style/identification.css">
+<div class=container-general>
+        <form method="post" action="formulaire.php" >
+        <div class="formulaire1 contact">
+            <label for="username">Identifiant</label>
+            <input name="username" type="text" placeholder="Identifiant" required  maxlenght="50">
+        </div>
+        <div class="formulaire2 contact">
+                <label for="pass">Mot de passe</label>
+                <input name="password" type="password" placeholder="Mot de passe" required  maxlenght="8">
+            </div>
+        
+        <button id='b2' class="boutton-envoyer" value="Submit">
+            <span>Envoyer</span>
+        </button>
+
+</form>
+    </div>
+</div>
