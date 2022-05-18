0ccab458e0e9103b9f3827fcd396f60f46a4d61a
Alexis Bonnet <a.bonnet@codeur.online>
2022-05-18 15:54:07 +0200

    Modifications présentation

--- PHP/presentation.php
+++ PHP/presentation.php
@@ -11,12 +11,11 @@
     </div>
     <div class="contener-tpresent cacher">
         <div class="text-info">
-        <p>Note est un site qui vous permettra de découvrir et apprendre les différents instruments.
-        Chez Note nous sommes passionnés de musique et voulons par l'information,
+        <p class="non-mobile">Note vous permet de découvrir et apprendre les différents instruments.</p>
+        <p>Chez Note nous sommes passionnés de musique et voulons – par l'information –
         sensibiliser le monde à la grande variété d'instruments de musique</p>
-        <H2 class="titre2">Laissez-vous allez a découvrir et redecouvrir nos instruments</H2>
-        
-        <p>Aidez nous a sensibiliser le monde a d'autre genre, d'autre style de musique</p>
+        <H2 class="titre2 ordinateur">Laissez-vous aller à découvrir et redecouvrir nos instruments</H2>
+        <p class="ordinateur">Aidez nous a sensibiliser le monde a d'autre genre, d'autre style de musique.</p>
         </div>
     </div>
 </div>
